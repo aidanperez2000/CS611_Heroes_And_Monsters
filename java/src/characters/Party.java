13fd61e8788277be748b1ba881a8ec8dbbbd1f5a
aidanperez2000 <aidan@perezwitt.com>
2025-11-16 16:44:33 -0500

    Add Party Class to GameEngine

---
+++ java/src/characters/Party.java
@@ -0,0 +1,43 @@
+package characters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Party {
+    public static final int MAX_PARTY_SIZE = 3;
+
+    private final List<Hero> heroes;
+
+    public Party() {
+        heroes = new ArrayList<>();
+    }
+
+    public boolean addHero(Hero hero) {
+        if (heroes.size() >= MAX_PARTY_SIZE) {
+            System.out.println("Party is full");
+            return false;
+        }
+        heroes.add(hero);
+        return true;
+    }
+
+    public Hero getHero(int heroId) {
+        return heroes.get(heroId);
+    }
+
+    public List<Hero> getHeroes() {
+        return heroes;
+    }
+
+    public int getHeroCount() {
+        return heroes.size();
+    }
+
+    public void showParty() {
+        System.out.println("Party:");
+        for (int i = 0; i < heroes.size(); i++) {
+            System.out.println((i + 1) + ". " + heroes.get(i).getName() + " HP: " + heroes.get(i).getHp()
+                + " Level: " + heroes.get(i).getLevel() + " Mana: " + heroes.get(i).getMana());
+        }
+    }
+}
--- java/src/game/GameEngine.java
+++ java/src/game/GameEngine.java
@@ -1,20 +1,26 @@
 package game;
 
 import characters.Hero;
+import characters.Party;
 import data.GameDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class GameEngine {
+    private Party party;
+
     public GameEngine() {
         // Load all game data ONCE when engine starts
         GameDatabase.LoadAll();
+        party = new Party();
     }
 
     public void run() {
         System.out.println("Welcome to Monsters and Heroes!");
         chooseHeroes();
+        party.showParty();
     }
 
     public void chooseHeroes() {
@@ -29,6 +35,24 @@ public class GameEngine {
         // Display them
         for (int i = 0; i < allHeroes.size(); i++) {
             System.out.println((i + 1) + ": " + allHeroes.get(i).getName());
+        }
+
+        Scanner input = new Scanner(System.in);
+        while (party.getHeroCount() < Party.MAX_PARTY_SIZE) {
+            System.out.println("Choose a hero!");
+
+            try {
+                int choice = Integer.parseInt(input.nextLine()) - 1;
+                if (choice >= 0 && choice < allHeroes.size()) {
+                    party.addHero(allHeroes.get(choice));
+                }
+                else {
+                    System.out.println("Invalid hero!");
+                }
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Invalid input!");
+            }
         }
     }
 }
